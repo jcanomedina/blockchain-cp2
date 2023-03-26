f092e74399a2338dadacfc58b8a5e9bd4e8f71f5
Javier Cano <javier.canomedina@yahoo.es>
2023-03-26 20:45:29 +0200

    Test folder removal

--- TESTS/MockExecutableProposal.sol
+++
@@ -1,8 +0,0 @@
-// MockExecutableProposal.sol
-pragma solidity ^0.8.0;
-
-import "../DAO/IExecutableProposal.sol";
-
-contract MockExecutableProposal is IExecutableProposal {
-    function executeProposal(uint propId, uint numVotes, uint budget) external override {}
-}
